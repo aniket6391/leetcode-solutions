7fb39d52abaf07208357ce261cb15a0276568612
aniket6391 <aniket6391915300@gmail.com>
2026-07-06 12:03:12 +0530

    Time: 12 ms (43.44%), Space: 11.8 MB (81.4%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,20 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-      if (head == NULL) return false;
+   if(head==NULL) return false;
+ListNode*slow=head;
+ListNode*fast=head;
 
-        ListNode* slow = head;
-        ListNode* fast = head;
+while(fast !=   NULL && fast->next !=   NULL){
+    slow= slow->next;
+    fast= fast->next->next;
 
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;          
-            fast = fast->next->next;    
-
-            if (slow == fast) {
-                return true;            
-            }
-        }
-
-        return false;    
+    if(slow==fast){
+        return true;
     }
-};
+}
+return false;
+}
+    };
